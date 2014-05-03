ef7dcce0f409614238b93e56cc7b5bf1e5655a25
Paul Conway <paulconway0@gmail.com>
2014-05-03 21:03:19 +0100

    Audio

--- SpaceWar/Assets/Scripts/ChaseState.cs
+++ SpaceWar/Assets/Scripts/ChaseState.cs
@@ -37,27 +37,26 @@ namespace BGE.States
 			if(enemyGameObject == null)
 			{
 				myGameObject.GetComponent<StateMachine>().SwitchState(new PatrolState(myGameObject));
-				shootTime += Time.deltaTime;
-				float fov = Mathf.PI / 4.0f;
-				float angle;
-				Vector3 toEnemy = (enemyGameObject.transform.position - myGameObject.transform.position);
-				toEnemy.Normalize();
-				angle = (float) Mathf.Acos(Vector3.Dot(toEnemy, myGameObject.transform.forward));
-				
-				if (angle < fov)
+			}
+			shootTime += Time.deltaTime;
+			float fov = Mathf.PI / 4.0f;
+			float angle;
+			Vector3 toEnemy = (enemyGameObject.transform.position - myGameObject.transform.position);
+			toEnemy.Normalize();
+			angle = (float) Mathf.Acos(Vector3.Dot(toEnemy, myGameObject.transform.forward));
+			
+			if (angle < fov)
+			{
+				if (shootTime > 0.25f)
 				{
-					if (shootTime > 0.25f)
-					{
-						GameObject laser = MonoBehaviour.Instantiate(laserGO, myGameObject.transform.position, Quaternion.identity)as GameObject;
-						laser.name = myGameObject.GetComponent<SteeringBehaviours>().laser.name;
-						laser.transform.position = myGameObject.transform.position;
-						laser.transform.forward = myGameObject.transform.forward;
-						
-						shootTime = 0.0f;
-					}
+					GameObject laser = MonoBehaviour.Instantiate(laserGO, myGameObject.transform.position, Quaternion.identity)as GameObject;
+					laser.name = myGameObject.GetComponent<SteeringBehaviours>().laser.name;
+					laser.transform.position = myGameObject.transform.position;
+					laser.transform.forward = myGameObject.transform.forward;
+					
+					shootTime = 0.0f;
 				}
 			}
-
 		}
 	}
 }
